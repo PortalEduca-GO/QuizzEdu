b20615ff08a62939214fe50e2e57b579b69bae48
PortalEduca-GO <portal.educa@educa.go.gov.br>
2025-07-23 11:34:59 -0300

    fix: corrige logs para variáveis VITE_SUPABASE_URL e VITE_SUPABASE_ANON_KEY

--- utils/supabaseClient.ts
+++ utils/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-console.log('SUPABASE_URL:', import.meta.env.VITE_SUPABASE_URL);
-console.log('SUPABASE_ANON_KEY:', import.meta.env.VITE_SUPABASE_ANON_KEY);
+console.log('VITE_SUPABASE_URL:', import.meta.env.VITE_SUPABASE_URL);
+console.log('VITE_SUPABASE_ANON_KEY:', import.meta.env.VITE_SUPABASE_ANON_KEY);
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL!;
 const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY!;
 
